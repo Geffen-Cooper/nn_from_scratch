74b5133a3143886934d881b395245815867a75d7
Geffen Cooper <geffenc2789@gmail.com>
2020-12-21 21:15:00 -0800

    having trouble with harder data

--- one_layer_network.py
+++ one_layer_network.py
@@ -1,6 +1,6 @@
-# This simple one layer network tries to learn how 2D points are distributed
-# For example, we may have "blue" points that are clustered in the left part
-# of the plane and "red" points that are clustered in the right part of the plane.
+# This very simple one layer network tries to learn how 2D points are distributed
+# For example, we have "blue" points that are clustered in the one part
+# of the plane and "red" points that are clustered in the other part of the plane.
 # The network should be able to learn these clusters and categorize a new point
 # not in the given training set
 
@@ -8,9 +8,13 @@
 # input: (x,y) coordinate 
 # output: categorization, 1 or 0
 
+# One issue with this network is that weigt and bias initialization is random.
+# If the parameters initialize to relatively large values the sigmoid will saturate
+# and the gradient won't change
+
 # import libs
 import numpy as np
-from sklearn.datasets import make_blobs
+from sklearn.datasets import make_blobs, make_moons, make_circles
 import matplotlib.pyplot as plt
 from matplotlib import image
 from pandas import DataFrame
@@ -26,8 +30,24 @@ def sigmoid(x):
 def sigmoid_derivative(x):
     return sigmoid(x)*(1-sigmoid(x))
 
+# activation functions and derivatives
+def relu(x):
+    return np.maximum(x,0)
+
+def relu_derivative(x):
+    return x > 0
+
+# use MSE loss with 1/2 in front
 def loss_derivative(a, y):
     return(a - y)
+
+def predict(x, y):
+    coord = np.array([x,y])
+    result = sigmoid(np.dot(W, coord) + B)
+    if(result < 0.5):
+        print("blue", (result))
+    else:
+        print("red", (result))
 
 
 # =========== Generate and visualize data ===============
@@ -35,8 +55,12 @@ def loss_derivative(a, y):
 # 2D dataset to classify
 # 100 points split into two clusters (centers) with 2 features (x,y coordinate) inside center_box
 # input is the list of points, output is the list of labels for each point
-sample_size = 100
+sample_size = 1000
 data_input, output = make_blobs(n_samples=sample_size, centers=2, n_features=2, center_box=(0,100), cluster_std=2.5)
+
+# more difficult data is moons or circles
+#data_input, output = make_moons(n_samples=sample_size, noise=0.1)
+#data_input, output = make_circles(n_samples=sample_size, noise=0.06, factor=0.1)
 
 # create a table from these inputs and outputs
 frame = DataFrame(dict(x_coord=data_input[:,0], y_coord=data_input[:,1], category=output))
@@ -53,6 +77,7 @@ for key, group in groups:
     group.plot(ax=ax, kind='scatter', x='x_coord', y='y_coord', label=key, color=labels[key])
 plt.show(block=False)
 
+
 # ============== neural network initialization ===================
 
 # transpose the input into column vectors
@@ -65,10 +90,11 @@ output = output.reshape(sample_size,1)
 np.random.seed(int(time.time())%1000)
 W = np.random.randn(1,2)/1000
 B = np.random.randn(1)/1000
-learning_rate = 0.05
+learning_rate = 0.04
+
 
 # ============== Train the Network ================
-for epoch in range(5000):
+for epoch in range(20000):
     # set the data_input
     X = data_input
 
@@ -76,14 +102,13 @@ for epoch in range(5000):
     # Step 1: Dot product of inputs with weight matrix
     # note we do this for all the inputs at once
     z = np.dot(W, X) + B
-    #print("z: ", z)
 
     # Step 2: pass through the activation function
     # a is now a list of all the outputs
     a = sigmoid(z)
 
     # ============= Backward Propagation ===============
-    # Step 1: Back prop through the output layer
+    # Back prop through the output layer
     loss = 0.5*(a.transpose()-output)**2
     print("loss: ", loss.sum())
 
@@ -107,38 +132,12 @@ for epoch in range(5000):
     B -= learning_rate*db
     W -= (learning_rate*dw).transpose()
 
+# print the weights and biases as a sanity check
 print("W: ", W)
 print("B: ", B)
 
-x_in = int(input("x:"))
-y_in = int(input("y:"))
-coord = np.array([x_in, y_in])
+# test the network
+x_in = float(input("x:"))
+y_in = float(input("y:"))
+predict(x_in, y_in)
 
-result = sigmoid(np.dot(W, coord) + B)
-
-if(result < 0.5):
-    print("blue", (result))
-else:
-    print("red", (result))
-
-x_in = int(input("x:"))
-y_in = int(input("y:"))
-coord = np.array([x_in, y_in])
-
-result = sigmoid(np.dot(W, coord) + B)
-
-if(result < 0.5):
-    print("blue", (result))
-else:
-    print("red", (result))
-
-x_in = int(input("x:"))
-y_in = int(input("y:"))
-coord = np.array([x_in, y_in])
-
-result = sigmoid(np.dot(W, coord) + B)
-
-if(result < 0.5):
-    print("blue", (result))
-else:
-    print("red", (result))
