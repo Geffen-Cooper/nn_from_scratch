76641835e80588d55763ce540cb33fcf6efad259
Geffen Cooper <geffencooper@gmail.com>
2022-07-24 00:29:57 -0500

    multilayer fc net backward test passed

--- tests/test_network.py
+++ tests/test_network.py
@@ -57,15 +57,58 @@ def create_fc_nets():
 
 
 def test_fc_network_forward():
+    # create the models
     torch_fc_net, numpynn_fc_net = create_fc_nets()
 
+    # create the input
     feature_d = numpynn_fc_net.layers[0].W.shape[1]
     batch_size = 2**rng.integers(0,5)
     torch_batch, numpynn_batch = create_random_batch((feature_d,batch_size))
 
+    # forward pass
     torch_out = torch_fc_net(torch_batch.T).detach().numpy().T
     numpynn_out = numpynn_fc_net.forward_pass(numpynn_batch)
 
     # implementations slightly different so assert precision threshold of 1e^-5
     out_diff = np.abs(torch_out - numpynn_out)
     assert (out_diff < prec_thresh).all()
+
+def test_fc_network_backward():
+    # create the models
+    torch_fc_net, numpynn_fc_net = create_fc_nets()
+
+    # create the loss
+    torch_mse = nn.MSELoss()
+    numpynn_mse = mse.MSELayer()
+
+    # create the input
+    feature_d = numpynn_fc_net.layers[0].W.shape[1]
+    batch_size = 2**rng.integers(0,5)
+    torch_batch, numpynn_batch = create_random_batch((feature_d,batch_size))
+
+    # create the label
+    feature_d = numpynn_fc_net.layers[-2].W.shape[0]
+    torch_label, numpynn_label = create_random_batch((feature_d,batch_size))
+
+    # forward pass
+    torch_out = torch_fc_net(torch_batch.T)
+    numpynn_out = numpynn_fc_net.forward_pass(numpynn_batch)
+
+    # calculate the loss
+    torch_loss = torch_mse(torch_out.T,torch_label)
+    numpynn_loss = numpynn_mse.forward(numpynn_out,numpynn_label) 
+
+    # backward pass
+    torch_loss.backward()
+    numpynn_fc_net.backward_pass(numpynn_mse.backward())
+
+    numpynn_fc = numpynn_fc_net.layers[0]
+    torch_fc = torch_fc_net.layers[0]
+
+    # compare the calculated gradients for each parameter up to a precision
+    diff_dW = np.abs(numpynn_fc.dW - torch_fc.weight.grad.numpy())
+    diff_dB = np.abs(numpynn_fc.dB - torch_fc.bias.grad.numpy().reshape(numpynn_fc.dB.shape))
+
+    # implementations slightly different so assert precision threshold of 1e^-5
+    assert ((diff_dW) < prec_thresh).all()
+    assert ((diff_dB) < prec_thresh).all()
